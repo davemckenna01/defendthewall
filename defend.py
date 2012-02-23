a17171633c4e597a07f9465139b414980c3e411c
Dave McKenna <davemckenna01@gmail.com>
2012-02-23 00:57:37 -0500

    initial commit

---
+++ defend.py
@@ -0,0 +1,17 @@
+from values import castles, brothers
+from operator import itemgetter
+import pprint
+
+
+#What a solution looks like:
+# [0,1,2,0,2,0,1,0,0,2,1,2,0,0,1,0,2,1,1,0,2,1,0...]
+castles
+#What the domain looks like
+# [(0,2),(0,2),(0,2),(0,2),(0,2),(0,2),(0,2)...]
+
+#Start by just evenly distributing brothers among
+#the castles.
+
+#After that, consider fighting style of each castle.
+
+
