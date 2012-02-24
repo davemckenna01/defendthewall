1a0ee5880e6629d4b296b4f9bc3ca0423411e40a
Dave McKenna <davemckenna01@gmail.com>
2012-02-23 19:41:44 -0500

    added a print fn to print solution

--- defend.py
+++ defend.py
@@ -1,5 +1,6 @@
 import random
 import optimization
+import pprint
 
 castles = [
   #                   % of fighting
@@ -49,8 +50,9 @@ brothers = [
 
 #What a solution looks like:
 # [0,1,2,0,2,0,1,0,0,2,1,2,0,0,1,0,2,1,1,0,2,1,0...]
-#where each index slot represents a particular brother
-#from the brothers list
+#where each list slot represents a particular brother
+#from the brothers list, and the value of a slot 
+#represents the castle that brother is assigned to
 
 #What the domain looks like
 # [(0,2),(0,2),(0,2),(0,2),(0,2),(0,2),(0,2)...]
@@ -63,8 +65,14 @@ randomvec = [random.randint(domain[i][0],domain[i][1])
 #Add up the total strength for each skill possessed by a brother
 totarrowstrength = sum([i[1] for i in brothers])
 totclosestrength = sum([i[2] for i in brothers])
-print "Total arrow strength of the Night's Watch: %s" % totarrowstrength
-print "Total close-range strength of the Night's Watch: %s " % totclosestrength
+
+#Ideal arrow and close strength value at each castle.
+#This is simple right now becuase we're just dividing total
+#strengths by # of castles. It will get more difficult
+#when we have to determine the fighting conditions of each
+#castle (i.e. how much is arrow fighting and how much close combat)
+optimalarrowstrength = int(totarrowstrength) / len(castles)
+optimalclosestrength = int(totclosestrength) / len(castles)
 
 def defendcost(vec):
   cost = 0
@@ -81,34 +89,41 @@ def defendcost(vec):
   for b in range(len(vec)):
     castles[vec[b]][3].append(b)
 
-  #Ideal arrow and close strength value at each castle.
-  #This is simple right now becuase we're just dividing total
-  #strengths by # of castles. It will get more difficult
-  #when we have to determine the fighting conditions of each
-  #castle (i.e. how much is arrow fighting and how much close combat)
-  optimalarrowstrength = int(totarrowstrength) / len(castles)
-  optimalclosestrength = int(totclosestrength) / len(castles)
-  #print "Optimal arrow strength: %s, optimal close strength: %s " % (optimalarrowstrength, optimalclosestrength)
-
   #Loop through the castles and determine how strength was 
   #distributed and calculate cost
   for c in range(len(castles)):
     castlebrothers = [brothers[i] for i in castles[c][3]]
-
     #Add up all the strength of the brothers in the castle
     solutionarrowstrength = sum([i[1] for i in castlebrothers])
     solutionclosestrength = sum([i[2] for i in castlebrothers])
-    #print "%s arrow strength: %s" % (castles[c][0], solutionarrowstrength)
-    #print "%s close strength: %s" % (castles[c][0], solutionclosestrength)
 
-    #Normalize the number to be always non-negativei - abs(). B/C whether 
+    #Normalize the number to be always non-negative: abs(). B/C whether 
     #the solution over or under supplies the castle is irrelevant (I think)
     cost += abs(optimalarrowstrength - solutionarrowstrength)
     cost += abs(optimalclosestrength - solutionclosestrength)
 
   return cost
 
+#Print out a solution (w/ names of brothers and castles, etc)
+def printdefense(vec):
+
+  #Make sure castles are empty
+  for c in range(len(castles)):
+    castles[c][3] = []
+
+  for b in range(len(vec)):
+    castles[vec[b]][3].append(b)
+  for castle in castles:
+    print "=== Castle: %s ===" % castle[0]
+    for bro in castle[3]:
+      print "%15s A: %2s  S: %2s" % (brothers[bro][0], brothers[bro][1], brothers[bro][2])
+    tarrow = sum([brothers[i][1] for i in castle[3]])
+    tclose = sum([brothers[i][2] for i in castle[3]])
+    print "Arrow strength at castle: %s" % tarrow
+    print "Close strength at castle: %s" % tclose
+
 #optimization.geneticoptimize(domain, defendcost)
 #optimization.annealingoptimize(domain, defendcost)
 #optimization.hillclimb(domain, defendcost)
 #optimization.randomoptimize(domain, defendcost)
+
